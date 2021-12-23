e69f8caf9b0343b25d1e4b81083156e62f5dc7c5
Simbak <simbak.netmind@gmail.com>
2021-12-23 06:21:11 -0700

    2. 4 Creating the User Sign-up Functionality 2

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,11 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityNetCore.Controllers
 {
+
+   
     public class IdentityController : Controller
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public async Task<IActionResult> Signup()
         {
             var model = new SignupViewModel();
@@ -15,9 +28,34 @@ namespace IdentityNetCore.Controllers
         [HttpPost]
         public async Task<IActionResult> Signup(SignupViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                if ((await _userManager.FindByEmailAsync(model.Email)) != null)
+                {
+                    var user = new IdentityUser
+                    {
+                        Email = model.Email,
+                        UserName = model.Email
+                    };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Signin");
+                    }
+
+                    ModelState.AddModelError("Signup", string.Join("", result.Errors.Select(x => x.Description)));
+                    return View(model);
+                }
+            }
 
             return View(model);
         }
+
+
+
+
+
+
 
         public async Task<IActionResult> Signin()
         {
