8c482f8030648e56f70acebedcb71eb2120a0235
Simbak <simbak.netmind@gmail.com>
2021-12-22 21:27:19 -0700

    2. 1 Implementing  Authentication and Authorization with  Local User Store
    1. Setting up the User Store (Database)

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityNetCore.Data
+{
+    public class ApplicationDBContext: IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
