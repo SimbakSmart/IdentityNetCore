911506b9abf5b83a97132e1ebdc3182aeddef49b
Simbak <simbak.netmind@gmail.com>
2021-12-27 13:41:59 -0700

    2.11  Implementing Sign-out Functionality and Creating a Navigation Bar

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -130,5 +130,11 @@ namespace IdentityNetCore.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Signout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Signin");
+        }
     }
 }
